953d995981cdfb815d0b09bc976f160fd5a6d957
jiaweizeng <597800295@qq.com>
2020-02-06 20:54:03 +0800

    第一次提交

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/pages/index_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        title: '百姓生活',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: IndexPage(),
+      ),
+    );
+  }
+}
+
